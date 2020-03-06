abf1ce60ca2405f42025ac1d85bb71186ecefabf
Your name <a.m.verdine@gmail.com>
2020-03-06 18:18:47 -0500

    Connect App component and pass props via mapStateToProps

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
